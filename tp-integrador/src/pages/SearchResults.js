4090bcbc0165c792438bb415b7e2468f0076161c
lucasaudouin <laudouin@udesa.edu.ar>
2024-09-23 18:10:41 -0300

    comienzo favoritos y detalle de pelicula

---
+++ tp-integrador/src/pages/SearchResults.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class SearchResults extends Component{
+    constructor (props){
+        super(props)
+    }
+    render() {
+        return (
+            <div>SearchResults {this.props.location.state.query}</div>
+          )
+        }
+
+}

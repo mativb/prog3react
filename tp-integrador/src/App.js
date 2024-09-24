f507f4c3f12e5a28cce8de8ade73c1d72753ea36
lucasaudouin <laudouin@udesa.edu.ar>
2024-09-24 09:35:37 -0300

    Realizo el detalle de las peliculas y arreglo favoritos

--- tp-integrador/src/App.js
+++ tp-integrador/src/App.js
@@ -2,11 +2,12 @@ import Footer from './components/Footer/Footer'
 import { Switch, Route} from "react-router-dom"
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home';
-import detallePelicula from './pages/detallePelicula';
+import DetallePelicula from './components/DetallePelicula/DetallePelicula.js';
 import mejorRating from './pages/MejorRating';
 import NotFound from "./pages/NotFound";
 import SearchForm from './components/SearchForm/SearchForm';
 import Proximamente from './pages/Proximamente';
+import Favoritos from './pages/Favoritos';
 function App() {
   return (
     <>
@@ -15,8 +16,9 @@ function App() {
     <SearchForm />
     <Switch>
       <Route path = "/" exact component ={Home}/>
-      <Route path =" /detalle/id/:id" exact component= {detallePelicula}/>
+      <Route path="/detalle/id/:IDpelicula" exact component={DetallePelicula} />
       <Route path ="/rating" exact component = {mejorRating}/>
+      <Route path ="/favoritos" exact component = {Favoritos}/>
       <Route path ="/proximamente" exact component = {Proximamente}/>
       <Route component={NotFound} />
     </Switch>
--- tp-integrador/src/pages/detallePelicula.js
+++ tp-integrador/src/pages/detallePelicula.js
@@ -1,7 +1,9 @@
+import DetallePelicula from "../components/DetallePelicula/DetallePelicula.js"
 const detallePelicula = () =>{
     return(
         <div> 
             <h1>Detalle de la peli:</h1>
+            <DetallePelicula/>
         </div>
     )
 }

2236e906a2d8a1ada2054a8eaded572c95279ab5
bautipeleritti <bpeleritti@udesa.edu.ar>
2024-09-25 19:44:36 -0300

    filtrado en las secciones ver todas

--- tp-integrador/src/components/VerTodas/VerTodas.js
+++ tp-integrador/src/components/VerTodas/VerTodas.js
@@ -3,12 +3,16 @@ import { Component } from "react";
 import Pelicula from "../Pelicula/Pelicula";
 import "./VerTodas.css"
 
+
+
 class VerTodas extends Component {
   constructor(props) {
     super(props)
     this.state = {
       pelis: [],
-      pelisAMostrar: 4
+      pelisAMostrar: 4,
+      filtradoPelis: [],
+      filtradoValue: ""
     }
   }
 
@@ -18,9 +22,13 @@ class VerTodas extends Component {
       .then((data) => {
         console.log(data)
         this.setState({
-          pelis: data.results
+          pelis: data.results,
+          filtradoPelis: data.results,
+
         })
-      })
+      }
+
+      )
   }
 
   mostrarMas() {
@@ -28,25 +36,43 @@ class VerTodas extends Component {
       pelisAMostrar: this.state.pelisAMostrar + 4
     });
   }
+  handleFilterChange(event) {
+    const userValue = event.target.value
+    this.setState({
+      filtradoValue: userValue,
+      filtradoPelis: this.state.pelis.filter(movie => movie.title.toLowerCase().includes(userValue.toLowerCase()))
+    })
+  }
+
+  handleResetFilter() {
+    this.setState({
+      filtradoValue: "",
+      filtradoPelis: this.state.pelis
+
+    })
+  }
 
   render(props) {
-    const { pelis, pelisAMostrar } = this.state;
+    const { filtradoPelis, pelisAMostrar } = this.state;
 
     return (
       <section className="vertodas_caja">
+        <input type="text" onChange={(event) => this.handleFilterChange(event)} value={this.state.filtradoValue} />
+        <button onClick={() => this.handleResetFilter()}>Resetear búsqueda</button>
         <div className="vertodas_lista">
-          {pelis.slice(0, pelisAMostrar).map((peli, index) => (
+          {filtradoPelis.slice(0, pelisAMostrar).map((peli, index) => (
             <Pelicula
               peli={peli.original_title}
               key={index}
               imagen={peli.poster_path}
               descripcion={peli.overview}
               id={peli.id}
+             
             />
           ))}
         </div>
 
-        {pelisAMostrar < pelis.length && (
+        {pelisAMostrar < filtradoPelis.length && (
           <button className="bot_ver_mas" onClick={() => this.mostrarMas()}>
             Ver más
           </button>
--- tp-integrador/src/pages/Home.js
+++ tp-integrador/src/pages/Home.js
@@ -4,10 +4,11 @@ import SearchForm from "../components/SearchForm/SearchForm";
 const Home = (props) => {
   return (
     <>
-    <SearchForm />
+    
     <div className="home">
+    <SearchForm history={props.history}/>
     <img src='./bannerci.jpg' alt=" banner"className="banner"/>
-      <h1 className="tituloHome">Películas con mejor rating</h1>
+      <h1 className="titulo_home">Películas con mejor rating</h1>
       <PeliculaGrid api={"https://api.themoviedb.org/3/movie/top_rated"} link={"/rating"} />
 
       <h1 className="titulo_home">Próximamente</h1>

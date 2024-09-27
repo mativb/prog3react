c6a6bd7b4ef447ade538383f80bf2babe4f2120d
lucasaudouin <laudouin@udesa.edu.ar>
2024-09-27 15:48:53 -0300

    paginas de favoritos y detalle terminadas con su css

--- tp-integrador/src/pages/Favoritos.js
+++ tp-integrador/src/pages/Favoritos.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class Favoritos extends Component {
     constructor(props) {
         super(props);
@@ -16,7 +16,6 @@ class Favoritos extends Component {
         const storage = localStorage.getItem('favoritos');  
         if (storage !== null) {
             const parsedArray = JSON.parse(storage);
-            // Usamos promise.all para obtener todas las películas
             Promise.all(
                 parsedArray.map((id) => {
                     return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=1f1b052c073a2b6b418269df7e8bb0c6`)
@@ -24,8 +23,10 @@ class Favoritos extends Component {
                 })
             )
             .then((movies) => {
+                const validMovies = movies.filter(movie => movie && movie.poster_path && movie.title && movie.id);
+
                 this.setState({
-                    movies: movies,
+                    movies: validMovies,
                     isLoading: false   
                 });
             })
@@ -56,20 +57,23 @@ class Favoritos extends Component {
         return (
             <div>
                 {!this.state.isLoading ? (
-                    <>Grilla de tus películas favoritas
+                    <>
                         {}
-                        <div className="movie-grid">
+                        <div className="favoritos-grid">
+                        <h2 className="titulofav">Grilla de tus películas favoritas:</h2>
+
                             {this.state.movies.length > 0 ? (this.state.movies.map((movie) => (
                                 <div key={movie.id} className="movie-item">
                                     <h3>{movie.title}</h3>
                                     <img src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`} alt={movie.title} />
-                                    <button onClick={() => this.sacarFavorito(movie.id)}>
+                                    <button onClick={() => this.sacarFavorito(movie.id)} className='eliminar-bot'>
                                             Eliminar de favoritos
                                         </button>
+                                        <Link to= { `/detalle/id/${movie.id}`} className='detallefav-link'>Ver detalle</Link>
                                 </div>
                             ))
                         ) : (
-                            <p>No tienes películas favoritas seleccionadas.</p>
+                            <p className='aviso-nopeli'>No tienes películas favoritas seleccionadas.</p>
                         )}
                             
                             
